f56421aca8aedb07eba7c5177fd4573ae72dd92b
puneeth.bharadwaj <puneeth.bharadwaj@mavs.uta.edu>
2014-10-17 09:01:29 -0500

    maps

---
+++ src/com/se/uta_rides/maps/MapsActivity.java
@@ -0,0 +1,15 @@
+package com.se.uta_rides.maps;
+
+import com.se.uta_rides.R;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MapsActivity extends FragmentActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_maps);
+	}
+}
