6a54092094058754e433718247e07b607d48140a
Shreekanth <shreekanth.kuppur@gmail.com>
2014-10-29 23:18:46 -0500

    Widget code

--- src/com/se/uta_rides/CarOwnerSetAvailbleActivity.java
+++ src/com/se/uta_rides/CarOwnerSetAvailbleActivity.java
@@ -9,11 +9,16 @@ import java.util.Date;
 import android.app.Activity;
 import android.app.TimePickerDialog;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Calendar;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -22,6 +27,7 @@ import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 
 import android.app.Activity;
 import android.app.TimePickerDialog;
@@ -45,13 +51,18 @@ import android.widget.Toast;
 public class CarOwnerSetAvailbleActivity extends Activity implements
 		OnClickListener, OnItemSelectedListener {
 	private Spinner dayDropDownList, favSpotDropDownList;
-	private Button buttonSave, buttonStartTime, buttonEndTime;
+	private Button buttonSave, buttonUpdate, buttonStartTime, buttonEndTime;
 	private EditText textStartTime, textEndTime;
 	Calendar calendar;
 	String selectedDate, selectedTime;
 	String selectedStartTime, selectedEndTime;
 	int mYear, mMonth, mDay, tHour, tMinute;
 	TimePickerDialog timePick;
+	HttpEntity entity;
+	InputStream isr;
+	JSONArray jArray;
+	String result, status1;
+	JSONArray jsonArray;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -82,7 +93,10 @@ public class CarOwnerSetAvailbleActivity extends Activity implements
 
 		dayDropDownList.setOnItemSelectedListener(this);
 		System.out.println(8);
+		buttonUpdate = (Button) findViewById(R.id.buttonUpdate);
+		buttonUpdate.setOnClickListener(this);
 
+		System.out.println(9);
 		ArrayAdapter<CharSequence> dayDropDownListAdapter = ArrayAdapter
 				.createFromResource(this, R.array.dayDropDownList,
 						android.R.layout.simple_spinner_item);
@@ -106,8 +120,7 @@ public class CarOwnerSetAvailbleActivity extends Activity implements
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.buttonSave:
-			
-			
+
 			Toast.makeText(
 					CarOwnerSetAvailbleActivity.this,
 					"You selected : "
@@ -128,7 +141,47 @@ public class CarOwnerSetAvailbleActivity extends Activity implements
 					String.valueOf(dayDropDownList.getSelectedItem()),
 					String.valueOf(favSpotDropDownList.getSelectedItem()),
 					selectedStartTime, selectedEndTime);
-			
+
+			break;
+
+		case R.id.buttonUpdate:
+			System.out.println("update");
+
+			UpdateData update = new UpdateData();
+
+			SharedPreferences userDetails1 = getSharedPreferences("MyData",
+					Context.MODE_PRIVATE);
+			String userName1 = userDetails1.getString("name", "null");
+
+			AsyncTask<String, String, String> checkTimings = update.execute(
+					userName1,
+					String.valueOf(dayDropDownList.getSelectedItem()),
+					String.valueOf(favSpotDropDownList.getSelectedItem()),
+					selectedStartTime, selectedEndTime);
+			try {
+				if (checkTimings.get() != "") {
+					System.out.println("timings not set set it first");
+					Toast.makeText(CarOwnerSetAvailbleActivity.this,
+							"Please save the timings before update", 2000)
+							.show();
+				} else {
+					Toast.makeText(
+							CarOwnerSetAvailbleActivity.this,
+							"You selected : "
+									+ "\n"
+									+ "Day : "
+									+ String.valueOf(dayDropDownList
+											.getSelectedItem())
+									+ "\n"
+									+ "Location : "
+									+ String.valueOf(favSpotDropDownList
+											.getSelectedItem()),
+							Toast.LENGTH_SHORT).show();
+				}
+			} catch (InterruptedException | ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
 			break;
 
@@ -147,7 +200,7 @@ public class CarOwnerSetAvailbleActivity extends Activity implements
 							System.out.println("... " + hourOfDay);
 							System.out.println(".... " + minute);
 							textStartTime.setText(hourOfDay + ":" + minute);
-							
+
 							selectedStartTime = hourOfDay + ":" + minute;
 						}
 					}, tHour, tMinute, false);
@@ -185,11 +238,11 @@ public class CarOwnerSetAvailbleActivity extends Activity implements
 	@Override
 	public void onItemSelected(AdapterView<?> parent, View view, int position,
 			long id) {
-//		if (position > 0) {
-//			Toast.makeText(parent.getContext(),
-//					"Saving your Preferences " + position + " ", Toast.LENGTH_SHORT)
-//					.show();
-//		}
+		// if (position > 0) {
+		// Toast.makeText(parent.getContext(),
+		// "Saving your Preferences " + position + " ", Toast.LENGTH_SHORT)
+		// .show();
+		// }
 	}
 
 	@Override
@@ -206,12 +259,13 @@ public class CarOwnerSetAvailbleActivity extends Activity implements
 				String email = params[0];
 				String day_id = params[1];
 				String loc = params[2];
-				String encodedLoc = URLEncoder.encode(loc, "UTF-8").replace("+", "%20");
+				String encodedLoc = URLEncoder.encode(loc, "UTF-8").replace(
+						"+", "%20");
 				String st = params[3];
 				String et = params[4];
-				String totimingsPHP = "email='" + email + "'&&" + "day_id='" + day_id
-						+ "'&&" + "loc='" + encodedLoc + "'&&" + "st='" + st
-						+ "'&&" + "et='" + et + "'";
+				String totimingsPHP = "email='" + email + "'&&" + "day_id='"
+						+ day_id + "'&&" + "loc='" + encodedLoc + "'&&"
+						+ "st='" + st + "'&&" + "et='" + et + "'";
 				httpClient = new DefaultHttpClient();
 				httpPost = new HttpPost(
 						"http://omega.uta.edu/~sxk7162/enter_timings.php?"
@@ -224,22 +278,96 @@ public class CarOwnerSetAvailbleActivity extends Activity implements
 
 			try {
 				HttpResponse httpResponse = httpClient.execute(httpPost);
-			} catch(UnsupportedEncodingException e){
-				Log.e("CarOwnerSetAvailableActivity URL Encode - ", e.toString());
-			}
-			catch (IllegalArgumentException e) {
-				Log.e("CarOwnerSetAvailableActivity Illegal Args - ", e.toString());
+			} catch (UnsupportedEncodingException e) {
+				Log.e("CarOwnerSetAvailableActivity URL Encode - ",
+						e.toString());
+			} catch (IllegalArgumentException e) {
+				Log.e("CarOwnerSetAvailableActivity Illegal Args - ",
+						e.toString());
 			} catch (HttpResponseException e) {
 				Log.e("CarOwnerSetAvailableActivity Response - ", e.toString());
 			} catch (ClientProtocolException e) {
 				Log.e("CarOwnerSetAvailableActivity Protocol - ", e.toString());
 			} catch (HttpHostConnectException e) {
-				Log.e("CarOwnerSetAvailableActivity Connection - ", e.toString());
+				Log.e("CarOwnerSetAvailableActivity Connection - ",
+						e.toString());
 			} catch (IOException e) {
 				Log.e("CarOwnerSetAvailableActivity IO - ", e.toString());
 			}
 
 			return null;
+		}
+	}
+
+	private class UpdateData extends AsyncTask<String, String, String> {
+		HttpClient httpClient;
+		HttpPost httpPost;
+
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				String email = params[0];
+				String day_id = params[1];
+				String loc = params[2];
+				String encodedLoc = URLEncoder.encode(loc, "UTF-8").replace(
+						"+", "%20");
+				String st = params[3];
+				String et = params[4];
+				String totimingsPHP = "email='" + email + "'&&" + "day_id='"
+						+ day_id + "'&&" + "loc='" + encodedLoc + "'&&"
+						+ "st='" + st + "'&&" + "et='" + et + "'";
+				httpClient = new DefaultHttpClient();
+				httpPost = new HttpPost(
+						"http://omega.uta.edu/~sxk7162/update_timings_check.php?"
+								+ totimingsPHP);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println("wooooooooo hooo");
+
+			try {
+				HttpResponse httpResponse = httpClient.execute(httpPost);
+				entity = httpResponse.getEntity();
+				if (entity != null) {
+					isr = entity.getContent();
+					System.out.println("byte - " + isr.available());
+				}
+			} catch (UnsupportedEncodingException e) {
+				Log.e("CarOwnerSetAvailableActivity URL Encode - ",
+						e.toString());
+			} catch (IllegalArgumentException e) {
+				Log.e("CarOwnerSetAvailableActivity Illegal Args - ",
+						e.toString());
+			} catch (HttpResponseException e) {
+				Log.e("CarOwnerSetAvailableActivity Response - ", e.toString());
+			} catch (ClientProtocolException e) {
+				Log.e("CarOwnerSetAvailableActivity Protocol - ", e.toString());
+			} catch (HttpHostConnectException e) {
+				Log.e("CarOwnerSetAvailableActivity Connection - ",
+						e.toString());
+			} catch (IOException e) {
+				Log.e("CarOwnerSetAvailableActivity IO - ", e.toString());
+			}
+			try {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(isr, "iso-8859-1"), 8);
+				StringBuilder sb = new StringBuilder();
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					sb.append(line);
+				}
+				isr.close();
+
+				result = sb.toString();
+				System.out.println("result from ISR : " + result);
+			} catch (Exception e) {
+				Log.e("log_tag", "Error converting result " + e.toString());
+			}
+
+			System.out.println("CarOwnerUpdateAvailability - " + result);
+
+			return result;
 		}
 	}
 }
